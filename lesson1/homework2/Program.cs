4c53409b94d3544b87caab64591bcb4e035d7285
Ivan Ashpin <ashpin.82@gmail.com>
2024-01-06 22:32:40 +0300

    Решение задачи 1

--- lesson1/homework2/Program.cs
+++ lesson1/homework2/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿int number = 6;
 
-int a = 5;
-int b =7;
-int max = b;
-int min = a;
-Console.WriteLine(max);
-Console.WriteLine(min);
+if (number % 2 == 0)
+{
+    Console.WriteLine("четное");
+    else
+    {
+        Console.WriteLine("нечетное");
+    }
+}
---
+++ lesson2/task1/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 161;
+
+if (a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine("кратно 7 и 23");
+    }
+else
+{
+    Console.WriteLine("не кратно 7 и 23");
+} 
