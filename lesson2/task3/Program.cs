070fdc8959ba4c93c5e5418b472779e008da6041
Ivan Ashpin <ashpin.82@gmail.com>
2024-01-07 07:22:49 +0300

    Решение задачи 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.

---
+++ lesson2/task3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход целое числ
+ // из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Random rnd = new Random();
+int a = rnd.Next(10, 99);
+Console.WriteLine(a);
+int num1 = a / 10;
+int num2 = a % 10;
+if (num1 > num2){Console.WriteLine(num1);}
+else {Console.WriteLine(num2);}
