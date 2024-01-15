993ff543154f48a3b072180e25ec4efef1370e20
IvanAshpin <Ashpin.82@gmail.com>
2024-01-15 23:38:14 +0300

    add task3 new

---
+++ lesson 2_2/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = { 1, 64, 3, 144, 6, 77, 8, 9, 6, 3 };
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($" {array[i]} ");
+    }
+    i = i + 1;
+}
