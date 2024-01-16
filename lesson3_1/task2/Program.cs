e4e9b2cbfd08824385740f2858da848b4395194c
IvanAshpin <Ashpin.82@gmail.com>
2024-01-17 00:29:42 +0300

    add lesson3_1:task2

---
+++ lesson3_1/task2/Program.cs
@@ -0,0 +1,40 @@
+﻿//int n = 5;
+
+//int i = 1;
+//while (i <= n)
+//{
+//    Console.Write($"{i * i} ");
+//    i = i + 1;
+//}
+
+//int n = 10;
+
+//int i = 1;
+//while (i <= n)
+//{
+//    Console.Write($"{i * i} ");
+//    i = i + 1;
+//}
+
+//int n = 15;
+
+//int i = 1;
+//while (i <= n)
+//{
+//    Console.Write($"{i * i} ");
+//    i = i + 1;
+//} 
+
+
+void PrintSqueras(int limit)
+{
+    int i = 1;
+    while(i <= limit)
+    {
+      Console.Write($"{i * i} ");
+    i++; //i = i + 1;  
+    }
+}
+PrintSqueras(5);
+PrintSqueras(10);
+PrintSqueras(15);
