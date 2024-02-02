25266da974adcd859f01ed416841b14e10f394f3
IvanAshpin <Ashpin.82@gmail.com>
2024-02-02 05:56:56 +0300

    add training

---
+++ training/Program.cs
@@ -0,0 +1,13 @@
+﻿//CompareNumbers(int firstNumber, int secondNumber);
+int firstNumber =19;
+int secondNumber = 8;
+
+{
+    if (firstNumber < secondNumber)
+        Console.WriteLine("Первое число `" + firstNumber + "` меньше чем второе число `" + secondNumber + "`");
+    else
+        if (firstNumber == secondNumber)
+        Console.WriteLine("Введенные числа равны `" + firstNumber + "`");
+    else
+        Console.WriteLine("Первое число `" + firstNumber + "` больше чем второе число `" + secondNumber + "`");
+}
