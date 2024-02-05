3a4f4143722b5caa23a7764d8f49963fbb9c6792
IvanAshpin <Ashpin.82@gmail.com>
2024-02-06 00:52:56 +0300

    add task3

---
+++ lesson6/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿{
+string input = "палатка";
+bool isPalindrome = IsPalindrome(input);
+Console.WriteLine(isPalindrome ? "Да" : "Нет");
+}
+static bool IsPalindrome(string str)
+{
+string normalized = new
+String(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+return normalized.SequenceEqual(normalized.Reverse());
+}
